501cc5a60cdab8084a67ac148aded93ce9f69809
Ignacio Yz <ignacioyz@gmail.com>
2022-05-03 19:46:43 -0400

    commit tree 8c4067cc114fb9446e15badd822ce9cdb443ccb3

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded",() => {
+    const par = document.getElementById('text')
+    par.textContent = 'This is really cool!'
+    console.log(par)
+
+})
+
+
